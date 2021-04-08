3db8e584b481dd95381e298c235cf37d130faeb3
Ojo Oluwasetemi <setemiojo@gmail.com>
2021-04-08 14:34:28 +0100

    🐛 FIX: backend not finding nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,37 +11,40 @@ const server = createServer();
 server.express.use(cookieParser());
 // TODO: Use express middleware to populate current user (JWT)
 server.express.use((req, res, next) => {
-  const { token } = req.cookies;
+	const { token } = req.cookies;
 
-  if (token) {
-    const { userId } = jwt.verify(token, process.env.APP_SECRET);
-    req.userId = userId;
-  }
-  next();
+	if (token) {
+		const { userId } = jwt.verify(token, process.env.APP_SECRET);
+		req.userId = userId;
+	}
+	next();
 });
 
 // 2. create a middleware that populates the user in the request
 server.express.use(async (req, res, next) => {
-  // if they aren't logged in, skip this
-  if (!req.userId) return next();
-  const user = await db.query.user(
-    {
-      where: { id: req.userId }
-    },
-    '{ id, permissions, email, name }'
-  );
-  req.user = user;
-  next();
+	// if they aren't logged in, skip this
+	if (!req.userId) return next();
+	const user = await db.query.user(
+		{
+			where: { id: req.userId },
+		},
+		'{ id, permissions, email, name }',
+	);
+	req.user = user;
+	next();
 });
 
 server.start(
-  {
-    cors: {
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
-  },
-  deets => {
-    console.log(`Server is now running on port http://localhost:${deets.port}`);
-  }
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL,
+		},
+	},
+	deets => {
+		console.log(
+			`Server is now running on port http://localhost:${process.env.PORT ||
+				deets.port}`,
+		);
+	},
 );
